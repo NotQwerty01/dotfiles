ca7c738dbafc60f851506153e5bbfd6f41de6d12
NotQwerty01 <eatacm@gmail.com>
2026-03-30 11:46:52 -0600

    Add files via upload

---
+++ fedora/nvim/lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		require 'conform'.setup({
+			formaters_by_ft = {
+				c = { "clangd-format" },
+				cpp = { "clang-format" },
+				css = { "css_beautify" },
+				html = { "html_beautify" },
+				lua = { "stylua" },
+				python = { "black" },
+				typescript = { "prettier" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+		})
+	end
+}
---
+++ fedora/nvim/lua/plugins/mini.lua
@@ -0,0 +1,9 @@
+return {
+	"nvim-mini/mini.nvim",
+	version = false,
+	config = function()
+		require("mini.clue").setup()
+		require("mini.cursorword").setup()
+		require("mini.indentscope").setup()
+	end
+}
