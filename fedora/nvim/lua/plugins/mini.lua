2b3fb8649b3773e629cfae4bc22db02e4d8efd28
NotQwerty01 <eatacm@gmail.com>
2026-04-24 12:54:48 -0600

    Delete fedora directory

--- fedora/nvim/lua/plugins/mini.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"nvim-mini/mini.nvim",
-	version = false,
-	config = function()
-		require("mini.clue").setup()
-		require("mini.cursorword").setup()
-		require("mini.indentscope").setup()
-	end
-}
--- fedora/nvim/lua/plugins/oil.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"stevearc/oil.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	lazy = false,
-	config = function()
-		require 'oil'.setup {}
-	end,
-}
